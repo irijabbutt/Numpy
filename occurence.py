b565b0df1282a29fcb7e2be8f43542ca572b63aa
RIJAB BUTT <rijabbutt23@gmail.com>
2024-04-27 22:54:02 +0500

    Count character occurences in string.

--- occurence.py
+++ occurence.py
@@ -1,11 +1,7 @@
 # occurences of a specific character in a string
-n=0
-char="a"
-text="akjdadjda"
-for i in text:
-    if char==i:
-        n=i      
-        print("Occurence: ",n) 
-    else:
-        print("char not present")
-
+def count_character_occurrences(string, char):
+ return string.count(char)
+test_string = "hello world"
+char_to_count = "l"
+print("Occurrences of", char_to_count, "in", test_string, ":",
+count_character_occurrences(test_string, char_to_count))
