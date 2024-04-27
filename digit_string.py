d50b24beccfc9e8b63176f6cfac8ced89023ee52
RIJAB BUTT <rijabbutt23@gmail.com>
2024-04-27 23:13:32 +0500

    not included files

--- digit_string.py
+++
@@ -1,5 +0,0 @@
-# check if a string contains only digits
-def digits(string):
- return string.isdigit()
-test_string = "1s323"
-print("Contains only digits:", digits(test_string))    
--- occurence.py
+++
@@ -1,7 +0,0 @@
-# occurences of a specific character in a string
-def count_character_occurrences(string, char):
- return string.count(char)
-test_string = "hello world"
-char_to_count = "l"
-print("Occurrences of", char_to_count, "in", test_string, ":",
-count_character_occurrences(test_string, char_to_count))
